54b28142f1216775b79e909f46b7f4554c221857
SauravSinha76 <sauravsinha76@gmail.com>
2023-02-03 07:30:22 +0530

    Update k_reverse_linked_list.py

--- class51/k_reverse_linked_list.py
+++ class51/k_reverse_linked_list.py
@@ -2,25 +2,39 @@ from class51.Node import Node, print_ll
 
 
 def reverse(A,B):
+    j = True
+    start = A
+    prv_start = start
+    head = None
+    curr = start
+    while curr :
+        prv = None
 
-    prv = None
-    curr = A
+        curr = start
 
-    i = 1
-    while i <= B:
-        tmp = curr.next
+        i = 1
+        while i <= B:
+            tmp = curr.next
 
-        curr.next = prv
-        prv = curr
-        curr = tmp
-        i += 1
-    return prv
+            curr.next = prv
+            prv = curr
+            curr = tmp
+            i += 1
+        if j :
+            head = prv
+            j = False
+        else:
+            prv_start.next = prv
+            prv_start = start
+        start = curr
+    return head
 
 head = Node(1)
 head.next = Node(2)
 head.next.next =Node(3)
 head.next.next.next =Node(4)
 head.next.next.next.next =Node(5)
+head.next.next.next.next.next =Node(6)
 print_ll(head)
 
 print_ll(reverse(head,2))
